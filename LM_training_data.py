be4b9b736f8204c1a69e1afc084b615ac2663582
PierceAndy <PierceAnderson.Fu+github@gmail.com>
2016-04-21 17:53:49 +0800

    Cleaning up directory structure

--- LM_training_data.py
+++ LM_training_data.py
@@ -2,10 +2,6 @@ from csv import DictReader
 import imdb
 
 
-
-
-
-
 def main():
 	imdb_movie_IDs = get_list_of_imdb_movie_IDs()
 	extract_genres_synopsis(imdb_movie_IDs)
@@ -15,9 +11,7 @@ def get_list_of_imdb_movie_IDs():
 	"""Gets lits of IMDB movie IDs to use with IMDB's online DB.
 	Src: http://grouplens.org/datasets/movielens/"""
 
-
-
-	csv_filepath = "csv/links.csv"
+	csv_filepath = "data/links.csv"
 
 	with open(csv_filepath) as csv_file:
 		imdb_movie_IDs = [row["imdbId"] for row in DictReader(csv_file)]
@@ -28,8 +22,8 @@ def get_list_of_imdb_movie_IDs():
 def extract_genres_synopsis(imdb_movie_IDs):
 	"""Extracts genres and the plot synopsis based on movie ID from IMDB"""
 
-	with open("LM_build.data", "w") as output_file, \
-			open("debug.txt", "w", 0) as debug_file:
+	with open("data/LM_build.data", "w") as output_file, \
+			open("debug/debug.txt", "w", 0) as debug_file:
 
 		for ID in imdb_movie_IDs:
 			try:
